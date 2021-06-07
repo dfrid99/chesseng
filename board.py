b11174167db311c5c5dc6fdb9e1a72bc0afac429
root <root@DESKTOP-9749OQN.localdomain>
2021-06-07 14:05:56 -0400

    add board

--- board.py
+++ board.py
@@ -1,1 +1,10 @@
+import chess.pgn
 
+pgn = open(r'\Users\dfrid\Downloads\chessgames\ficsgamesdb_search_208912.pgn')
+
+first_game = chess.pgn.read_game(pgn)
+board = first_game.board()
+
+for move in first_game.mainline_moves():
+    board.push(move)
+    print(board)
